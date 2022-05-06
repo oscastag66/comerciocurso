c0d458b528bec15c90ffdc3c0064b23a6d52cbf4
unknown <oscastag66@hotmail.com>
2022-05-06 11:41:57 -0500

    Repositorio Listo para Subir a Github

---
+++ backend/main/models/__init__.py
@@ -0,0 +1,6 @@
+from .Usuario import Usuario as UsuarioModel
+from .Producto import Producto as ProductoModel
+from .Compra import Compra as CompraModel
+from .ProductoCompra import ProductoCompra as ProductoCompraModel
+
+
---
+++ backend/main/resources/__init__.py
@@ -0,0 +1,10 @@
+from .Clientes import Clientes as ClientesResource
+from .Clientes import Cliente as ClienteResource
+from .Usuarios import Usuarios as UsuariosResource
+from .Usuarios import Usuario as UsuarioResource
+from .Productos import Productos as ProductosResource
+from .Productos import Producto as ProductoResource
+from .Compras import Compras as ComprasResource
+from .Compras import Compra as CompraResource
+from .ProductosCompras import ProductosCompras as ProductosComprasResource
+from .ProductosCompras import ProductoCompra as ProductoCompraResource
